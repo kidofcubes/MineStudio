8b4eb326ba7b19c1d0d35302f6c4566d672b6560
seanspt <>
2025-06-15 16:09:41 +0800

    laucnClient with java when on mac

--- minestudio/simulator/minerl/env/launchClient.sh
+++ minestudio/simulator/minerl/env/launchClient.sh
@@ -35,7 +35,11 @@ if [ \( $port -lt 0 \) -o \( $port -gt 65535 \) ]; then
 fi
 
 if [ "$device" == "cpu" ]; then
-    xvfb-run -a java -Xmx$maxMem -jar $fatjar --envPort=$port
+    if [ `uname` == 'Darwin' ]; then
+        java -Xmx$maxMem -XstartOnFirstThread -jar $fatjar --envPort=$port --envSeed=$seed
+    else
+        xvfb-run -a java -Xmx$maxMem -jar $fatjar --envPort=$port
+    fi
 else
     vglrun -d $device java -Xmx$maxMem -jar $fatjar --envPort=$port
 fi
